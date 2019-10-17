ae80561e3341df5b7b4c3857ca0b31cc39286ef4
Mistralys <s.mordziol@gmail.com>
2019-10-17 17:01:02 +0200

    Extracted HTML tags from the string.

--- example/sources/php/variables.php
+++ example/sources/php/variables.php
@@ -2,6 +2,6 @@
 
     $text = t('Found %1$s items in %2$s pages.', 50, 6);
     
-    $text = t('There are <b>%s</b> fish in the sea.', 'many');
+    $text = t('There are %1$s%2$s%3$s fish in the sea.', '<b>', 'many', '</b>');
     
     $text = t('Zero-padded variables like %03d are also recognized.', 5);
