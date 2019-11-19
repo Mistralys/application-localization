316542a1c66018b1121bb68c38d4f4672eb4404e
Mistralys <s.mordziol@gmail.com>
2019-11-19 15:43:27 +0100

    Added translation tests.

---
+++ tests/testsuites/Translator.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use AppLocalize\Localization;
+
+final class TranslatorTest extends TestCase
+{
+    protected function setUp() : void
+    {
+        Localization::reset();
+    }
+    
+   /**
+    * Ensure that the bundled translations work out of the box.
+    */
+    public function test_bundledTexts()
+    {
+        Localization::addAppLocale('de_DE');
+        
+        Localization::selectAppLocale('de_DE');
+        
+        $this->assertEquals('Deutschland', Localization::getAppLocale()->getCountry()->getLabel());
+    }
+}
