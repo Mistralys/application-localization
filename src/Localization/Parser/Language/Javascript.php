96dafd7a8827f2434550427f6f1528e315f5ce91
Mistralys <s.mordziol@gmail.com>
2021-08-05 22:07:18 +0200

    Added a use statement.

--- src/Localization/Parser/Language/Javascript.php
+++ src/Localization/Parser/Language/Javascript.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace AppLocalize;
 
+use JTokenizer\JTokenizer;
+
 class Localization_Parser_Language_Javascript extends Localization_Parser_Language
 {
     protected function getTokens() : array
     {
-        return \JTokenizer\JTokenizer::getTokens($this->content);
+        return JTokenizer::getTokens($this->content);
     }
 }
