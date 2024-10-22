82f7415c040e67c8373caa4f71fefd9f51de0c47
Mistralys <s.mordziol@gmail.com>
2024-10-22 12:25:49 +0200

    Deprecated classes for backwards compatibility.

---
+++ src/_deprecated/CountryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppLocalize;
+
+use AppLocalize\Localization\Countries\CountryInterface;
+
+/**
+ * @package Localization
+ * @subpackage Countries
+ * @deprecated Use {@see CountryInterface} instead.
+ */
+interface Localization_CountryInterface extends CountryInterface
+{
+
+}
---
+++ src/_deprecated/CurrencyNumber.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Localization
+ * @subpackage Currencies
+ */
+
+use AppLocalize\Localization\Currencies\CurrencyNumberInfo;
+
+/**
+ * @package Localization
+ * @subpackage Currencies
+ * @deprecated Use {@see CurrencyNumberInfo} instead.
+ */
+class Localization_Currency_Number extends CurrencyNumberInfo
+{
+
+}
+
