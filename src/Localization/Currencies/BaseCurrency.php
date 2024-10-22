cfea3e7c73a6f8c1c6a5b57716fa54d9617b1f04
Mistralys <s.mordziol@gmail.com>
2024-10-22 13:30:04 +0200

    Updated the currency interface.

---
+++ src/Localization/Currencies/BaseCurrency.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package Localization
+ * @subpackage Currencies
+ */
+
+declare(strict_types=1);
+
+namespace AppLocalize\Localization\Currencies;
+
+use AppLocalize\Localization;
+use AppLocalize\Localization\Countries\CountryInterface;
+
+/**
+ * Individual currency representation.
+ *
+ * @package Localization
+ * @subpackage Currencies
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+abstract class BaseCurrency implements CurrencyInterface
+{
+    /**
+     * Creates a new currency object.
+     *
+     * @param string $id
+     * @return CurrencyInterface
+     * @deprecated Use the collection {@see Localization::createCurrencies()} instead.
+     */
+    public static function create(string $id) : CurrencyInterface
+    {
+        return Localization::createCurrencies()->getByID($id);
+    }
+
+    public function __construct()
+    {
+    }
+
+    public function getID(): string
+    {
+        return $this->getISO();
+    }
+
+    /**
+     * Gets all countries that this currency is used in.
+     * @return CountryInterface[]
+     */
+    public function getCountries() : array
+    {
+        $countries = array();
+        $iso = $this->getISO();
+
+        foreach(Localization::createCountries()->getAll() as $country) {
+            if($country->getCurrencyISO() === $iso) {
+                $countries[] = $country;
+            }
+        }
+
+        return $countries;
+    }
+
+    /**
+     * Checks whether the specified currency name is known
+     * (supported by the application)
+     *
+     * @param string $currencyName Currency code, e.g. {@see Localization_Currency_USD::ISO_CODE}.
+     * @return boolean
+     * @deprecated Use {@see CurrencyCollection::idExists()} instead.
+     */
+    public static function isCurrencyKnown(string $currencyName) : bool
+    {
+        return Localization::createCurrencies()->idExists($currencyName);
+    }
+
+    /**
+     * Returns the singular of the currency label.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getSingular();
+    }
+
+    public function getPreferredSymbol(): string
+    {
+        if($this->isNamePreferred()) {
+            return $this->getISO();
+        }
+
+        return $this->getSymbol();
+    }
+}
---
+++ src/Localization/Currencies/CurrencyInterface.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppLocalize\Localization\Currencies;
+
+use AppLocalize\Localization\Countries\CountryInterface;
+use AppLocalize\Localization_Currency_EUR;
+use AppUtils\Interfaces\StringableInterface;
+use AppUtils\Interfaces\StringPrimaryRecordInterface;
+
+interface CurrencyInterface
+    extends
+    StringableInterface,
+    StringPrimaryRecordInterface
+{
+    /**
+     * The currency ISO code, e.g. {@see Localization_Currency_EUR::ISO_CODE}.
+     */
+    public function getID() : string;
+
+    /**
+     * @return CountryInterface[]
+     */
+    public function getCountries() : array;
+
+    /**
+     * The singular label of the currency, e.g. "Dollar", "Pound"
+     * @return string
+     */
+    public function getSingular() : string;
+
+    /**
+     * The plural label of the currency, e.g. "Dollars", "Pounds"
+     * @return string
+     */
+    public function getPlural() : string;
+
+    /**
+     * The currency symbol, e.g. "$", "€"
+     * @return string
+     */
+    public function getSymbol() : string;
+
+    public function getPreferredSymbol() : string;
+
+    public function getISO() : string;
+
+    /**
+     * Gets the formatting template for the currency.
+     * @param CountryInterface|NULL $country Some currencies have different formatting based on the country.
+     *      Add the country to guarantee the matching formatting, otherwise the default formatting is returned.
+     * @return string
+     */
+    public function getStructuralTemplate(?CountryInterface $country=null) : string;
+
+    /**
+     * Whether the symbol is typically shown at the beginning of the amount.
+     * @return bool
+     */
+    public function isSymbolOnFront() : bool;
+
+    /**
+     * Whether the currency name is preferred over the symbol.
+     * @return bool
+     */
+    public function isNamePreferred() : bool;
+}
