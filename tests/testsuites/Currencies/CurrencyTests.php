b463ce27a9b3f2498b15a0be9462c9b66b3de560
Mistralys <s.mordziol@gmail.com>
2024-10-22 13:31:13 +0200

    Added tests.

---
+++ tests/testsuites/Currencies/CurrencyTests.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace testsuites\Currencies;
+
+use AppLocalize\Localization\Countries\CountryCollection;
+use AppLocalize\Localization\Countries\CountryInterface;
+use AppLocalize\Localization\Currencies\CurrencyCollection;
+use AppLocalize\Localization\Currencies\CurrencyInterface;
+use AppLocalize\Localization_Currency_EUR;
+use AppLocalize\Localization_Currency_USD;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTests extends TestCase
+{
+    public function test_getByID() : void
+    {
+        $usd = CurrencyCollection::getInstance()->choose()->usd();
+
+        $this->assertSame('USD', $usd->getISO());
+        $this->assertTrue($usd->isSymbolOnFront());
+    }
+
+    public function test_EUR_getCountries() : void
+    {
+        $countries = CountryCollection::getInstance();
+        $currency = CurrencyCollection::getInstance()->choose()->eur();
+
+        $this->assertCount(5, $currency->getCountries());
+        $this->assertCurrencyHasCountry($currency, $countries->choose()->de());
+        $this->assertCurrencyHasCountry($currency, $countries->choose()->es());
+        $this->assertCurrencyHasCountry($currency, $countries->choose()->fr());
+        $this->assertCurrencyHasCountry($currency, $countries->choose()->it());
+        $this->assertCurrencyHasCountry($currency, $countries->choose()->at());
+    }
+
+    public function assertCurrencyHasCountry(CurrencyInterface $currency, CountryInterface $country) : void
+    {
+        $targetCode = $country->getCode();
+
+        $found = false;
+        $codes = array();
+        foreach($currency->getCountries() as $c) {
+            $codes[] = $c->getCode();
+            if($c->getCode() === $targetCode) {
+                $found = true;
+            }
+        }
+
+        $this->assertTrue(
+            $found,
+            sprintf(
+                'The currency [%s] does not have the country [%s]. '.PHP_EOL.
+                'Available countries are: '.PHP_EOL.
+                '- %s',
+                $currency->getISO(),
+                $country->getCode(),
+                implode(PHP_EOL.'- ', $codes)
+            )
+        );
+    }
+}
---
+++ tests/testsuites/Currencies/FormattingTests.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace testsuites\Currencies;
+
+use AppLocalize\Localization\Countries\CountryCollection;
+use AppLocalize\Localization_Country_CA;
+use AppLocalize\Localization_Country_DE;
+use AppLocalize\Localization_Country_FR;
+use AppLocalize\Localization_Country_MX;
+use AppLocalize\Localization_Country_UK;
+use AppLocalize\Localization_Country_US;
+use PHPUnit\Framework\TestCase;
+
+class FormattingTests extends TestCase
+{
+    public function test_US_USD() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->us()->getCurrency();
+
+        $this->assertSame('USD', $currency->getISO());
+        $this->assertSame('1,445.45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1,445.45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445,45'));
+        $this->assertSame('$1,445.45', $currency->makeReadable(1445.45));
+        $this->assertSame('-$1,445.45', $currency->makeReadable(-1445.45));
+    }
+
+    public function test_DE_EUR() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->de()->getCurrency();
+
+        $this->assertSame('EUR', $currency->getISO());
+        $this->assertSame('1.445,45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1.445,45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445.45'));
+        $this->assertSame('1.445,45 €', $currency->makeReadable(1445.45));
+        $this->assertSame('-1.445,45 €', $currency->makeReadable(-1445.45));
+    }
+
+    public function test_FR_EUR() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->fr()->getCurrency();
+
+        $this->assertSame('EUR', $currency->getISO());
+        $this->assertSame('1 445,45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1.445,45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445.45'));
+        $this->assertSame('1 445,45 €', $currency->makeReadable(1445.45));
+        $this->assertSame('- 1 445,45 €', $currency->makeReadable(-1445.45));
+    }
+
+    public function test_MX_MXN() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->mx()->getCurrency();
+
+        $this->assertSame('MXN', $currency->getISO());
+        $this->assertSame('1,445.45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1.445,45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445.45'));
+        $this->assertSame('MXN 1,445.45', $currency->makeReadable(1445.45));
+        $this->assertSame('MXN -1,445.45', $currency->makeReadable(-1445.45));
+    }
+
+    public function test_CA_CAD() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->ca()->getCurrency();
+
+        $this->assertSame('CAD', $currency->getISO());
+        $this->assertSame('1,445.45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1.445,45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445.45'));
+        $this->assertSame('$ 1,445.45', $currency->makeReadable(1445.45));
+        $this->assertSame('$ -1,445.45', $currency->makeReadable(-1445.45));
+    }
+
+    public function test_UK_GBP() : void
+    {
+        $currency = CountryCollection::getInstance()->choose()->uk()->getCurrency();
+
+        $this->assertSame('GBP', $currency->getISO());
+        $this->assertSame('1,445.45', $currency->formatNumber(1445.45));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1.445,45'));
+        $this->assertSame('1445.45', $currency->normalizeNumber('1 445.45'));
+        $this->assertSame('£1,445.45', $currency->makeReadable(1445.45));
+        $this->assertSame('-£1,445.45', $currency->makeReadable(-1445.45));
+    }
+}
