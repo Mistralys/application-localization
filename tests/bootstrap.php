2e396aaf4b43f98ff366f2504ffccda52f90accb
Mistralys <s.mordziol@gmail.com>
2025-05-16 16:29:58 +0200

    Added the class helper cache folder.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,43 +7,47 @@
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    use AppLocalize\Localization;
+use AppLocalize\Localization;
+use AppUtils\ClassHelper;
 
-    const TESTS_ROOT = __DIR__;
+const TESTS_ROOT = __DIR__;
 
-    $autoloader = realpath(TESTS_ROOT.'/../vendor/autoload.php');
-    if($autoloader === false) {
-        die('Cannot run tests: the autoload is not present. Please run composer update first.');
-    }
-    
-    $sourcesPath = realpath(__DIR__.'/../example/sources');
-    if($sourcesPath === false) {
-        die('Cannot run tests: the example sources could not be found.');
-    }
-    
-    // where the test files are stored
-    $storageFolder = TESTS_ROOT.'/storage';
-    
-    // where the source files analysis cache should be stored
-    $storageFile = $storageFolder.'/storage.json';
-    
-    // where the javascript includes should be written
-    $librariesFolder = $storageFolder;
-    
-    // register the sources folder.
-    $source = Localization::addSourceFolder(
-        'main',
-        'Main translation texts',
-        'Core files',
-        $storageFolder,
-        $sourcesPath
-    );
-    
-    // this folder will be ignored when searching for source files to analyze
-    $source->excludeFolder('excludeme');
-    
-    // has to be called last after all sources and locales have been configured
-    Localization::configure($storageFile, $librariesFolder);
-    
+$autoloader = __DIR__.'/../vendor/autoload.php';
+if(!file_exists($autoloader)) {
+    die('Cannot run tests: the autoload is not present. Please run composer update first.');
+}
+
+$sourcesPath = __DIR__.'/../example/sources';
+if(!is_dir($sourcesPath)) {
+    die('Cannot run tests: the example sources could not be found.');
+}
+
+require_once $autoloader;
+
+ClassHelper::setCacheFolder(__DIR__.'/cache');
+
+// where the test files are stored
+$storageFolder = TESTS_ROOT.'/storage';
+
+// where the source files analysis cache should be stored
+$storageFile = $storageFolder.'/storage.json';
+
+// where the JavaScript includes should be written
+$librariesFolder = $storageFolder;
+
+// register the folder for the code sources.
+$source = Localization::addSourceFolder(
+    'main',
+    'Main translation texts',
+    'Core files',
+    $storageFolder,
+    $sourcesPath
+);
+
+// this folder will be ignored when searching for source files to analyze
+$source->excludeFolder('excludeme');
+
+// has to be called last after all sources and locales have been configured
+Localization::configure($storageFile, $librariesFolder);
