1cab67975f226358c17525e447c4d327d3c53234
Mistralys <s.mordziol@gmail.com>
2025-05-19 08:28:38 +0200

    Deprecated exception class.

---
+++ src/_deprecated/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppLocalize;
+
+use AppLocalize\Localization\LocalizationException;
+use AppUtils\BaseException;
+
+/**
+ * @deprecated Use {@see LocalizationException} instead.
+ */
+class Localization_Exception extends BaseException
+{
+}
