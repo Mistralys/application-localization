6696dc44d24b966f6112113ef6541304feece9c9
Mistralys <s.mordziol@gmail.com>
2025-05-20 13:53:52 +0200

    Improved code somewhat.

--- localization/index.php
+++ localization/index.php
@@ -6,29 +6,32 @@
  * @subpackage Examples
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
-    
-    $root = __DIR__;
-    $installFolder = realpath($root.'/../');
-    
-    $autoload = realpath($root.'/../vendor/autoload.php');
-    
-    // we need the autoloader to be present
-    if(!file_exists($autoload)) {
-        die('<b>ERROR:</b> Autoloader not present. Run composer update first.');
-    }
-    
-    /**
-     * The composer autoloader
-     */
-    require_once $autoload;
-    
-    // add the locales we wish to manage (en_UK is always present)
-    \AppLocalize\Localization::addAppLocale('de_DE');
-    \AppLocalize\Localization::addAppLocale('fr_FR');
-    
-    // has to be called last after all sources and locales have been configured
-    \AppLocalize\Localization::configure($root.'/storage.json', '');
-    
-    // create the editor UI and start it
-    $editor = \AppLocalize\Localization::createEditor();
-    $editor->display();
+
+declare(strict_types=1);
+
+use AppLocalize\Localization;
+
+$installFolder = __DIR__.'/../';
+
+$autoload = __DIR__.'/../vendor/autoload.php';
+
+// we need the autoloader to be present
+if(!file_exists($autoload)) {
+    die('<b>ERROR:</b> Autoloader not present. Run composer update first.');
+}
+
+/**
+ * The composer autoloader
+ */
+require_once $autoload;
+
+// add the locales we wish to manage (en_US is always present)
+Localization::addAppLocale('de_DE');
+Localization::addAppLocale('fr_FR');
+
+// has to be called last after all sources and locales have been configured
+Localization::configure(__DIR__.'/storage.json', '');
+
+// create the editor UI and start it
+$editor = Localization::createEditor();
+$editor->display();
