5c16d0d516423da00d9952d4ab2c359e8480e56b
Mistralys <s.mordziol@gmail.com>
2025-05-20 13:48:43 +0200

    Replaced `en_UK` locale with correct `en_GB`.

--- src/Localization/Locale/en_GB.php
+++ src/Localization/Locale/en_GB.php
@@ -7,22 +7,30 @@ namespace AppLocalize\Localization\Locale;
 use AppLocalize\Localization\Locales\BaseLocale;
 use function AppLocalize\t;
 
-class en_UK extends BaseLocale
+class en_GB extends BaseLocale
 {
-    public const LOCALE_NAME = 'en_UK';
+    public const LOCALE_NAME = 'en_GB';
+    public const LOCALE_ALIAS_UK = 'en_UK';
 
     public function getName() : string
     {
         return self::LOCALE_NAME;
     }
 
+    public function getAliases(): array
+    {
+        return array(
+            self::LOCALE_ALIAS_UK
+        );
+    }
+
     public function getLabel() : string
     {
-        return t('English (UK)');
+        return t('English (Great Britain)');
     }
 
     public function getLabelInvariant() : string
     {
-        return 'English (UK)';
+        return 'English (Great Britain)';
     }
 }
