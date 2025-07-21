444d7b032b5205b6b0d603b3ec32fc1d10fef3dd
Mistralys <s.mordziol@gmail.com>
2025-07-21 14:40:29 +0200

    Added PHPDocs.

--- src/Localization/Locale/de_DE.php
+++ src/Localization/Locale/de_DE.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package Localization
+ * @subpackage Locales
+ */
 
 declare(strict_types=1);
 
@@ -8,6 +12,12 @@ use AppLocalize\Localization\Locales\BaseLocale;
 use function AppLocalize\t;
 use function AppUtils\sb;
 
+/**
+ * Locale for German (Germany).
+ *
+ * @package Localization
+ * @subpackage Locales
+ */
 class de_DE extends BaseLocale
 {
     public const LOCALE_NAME = 'de_DE';
