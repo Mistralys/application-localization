06fe360080613221c594787dde6dadec03eb3537
Mistralys <s.mordziol@gmail.com>
2025-07-25 14:46:41 +0200

    Added an error constant.

--- src/Localization/Countries/CountryException.php
+++ src/Localization/Countries/CountryException.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package Localization
+ * @subpackage Countries
+ */
 
 declare(strict_types=1);
 
@@ -6,7 +10,14 @@ namespace AppLocalize\Localization\Countries;
 
 use AppLocalize\Localization\LocalizationException;
 
+/**
+ * Exception and exception codes for the country localization module.
+ *
+ * @package Localization
+ * @subpackage Countries
+ */
 class CountryException extends LocalizationException
 {
     public const ERROR_CANNOT_PARSE_CURRENCY_NUMBER = 177701;
+    public const ERROR_INVALID_BASKET_COUNTRY_SELECTION = 177702;
 }
