7a646036ebd702356f18f47755ada5e9c356b2cf
Mistralys <s.mordziol@gmail.com>
2025-07-25 14:46:29 +0200

    Added the country basket utility class.

---
+++ src/Localization/Countries/CountryBasket.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Localization
+ * @subpackage Countries
+ */
+
+declare(strict_types=1);
+
+namespace AppLocalize\Localization\Countries;
+
+use AppUtils\Baskets\GenericStringPrimaryBasket;
+
+/**
+ * Country basket utility class: A collection of countries with
+ * methods to freely add and remove them.
+ *
+ * @package Localization
+ * @subpackage Countries
+ *
+ * @method CountryInterface[] getAll()
+ * @method CountryInterface getByID(string $id)
+ * @method static CountryBasket create()
+ */
+class CountryBasket extends GenericStringPrimaryBasket
+{
+    public function getAllowedItemClasses(): array
+    {
+        return array(
+            CountryInterface::class
+        );
+    }
+}
