f606ce01c3067a03771a86dfdfac66c7f847ebff
Mistralys <s.mordziol@gmail.com>
2025-07-28 11:42:22 +0200

    Added TZ tests.

--- tests/testsuites/TimeZones/TimeZoneTests.php
+++ tests/testsuites/TimeZones/TimeZoneTests.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace AppLocalize\tests\testsuites\TimeZones;
 
+use AppLocalize\Localization\Countries\CountryCollection;
+use AppLocalize\Localization\Country\CountryAT;
+use AppLocalize\Localization\Country\CountryBE;
+use AppLocalize\Localization\Country\CountryCH;
+use AppLocalize\Localization\Country\CountryDE;
+use AppLocalize\Localization\Country\CountryES;
+use AppLocalize\Localization\Country\CountryFI;
+use AppLocalize\Localization\Country\CountryFR;
+use AppLocalize\Localization\Country\CountryGB;
+use AppLocalize\Localization\Country\CountryIE;
+use AppLocalize\Localization\Country\CountryIT;
+use AppLocalize\Localization\Country\CountryNL;
+use AppLocalize\Localization\Country\CountryPL;
+use AppLocalize\Localization\Country\CountryRO;
+use AppLocalize\Localization\Country\CountrySE;
+use AppLocalize\Localization\Locales\LocalesCollection;
+use AppLocalize\Localization\TimeZone\Globals\GlobalCETTimeZone;
+use AppLocalize\Localization\TimeZone\Globals\GlobalUTCTimeZone;
 use AppLocalize\Localization\TimeZones\TimeZoneCollection;
 use PHPUnit\Framework\TestCase;
 
@@ -72,5 +90,51 @@ class TimeZoneTests extends TestCase
             $this->assertNotNull($zone, "Time zone not found for locale: $locale");
             $this->assertSame($timezone, $zone->getID(), "Time zone should be $timezone for locale $locale, but got " . $zone->getID());
         }
+    }
+
+    public function test_UTCTimeZoneContainsAllKnownCountries() : void
+    {
+        $timeZone = TimeZoneCollection::getInstance()->getByID(GlobalUTCTimeZone::ZONE_ID);
+
+        $this->assertInstanceOf(GlobalUTCTimeZone::class, $timeZone);
+
+        $this->assertSame(CountryCollection::getInstance()->countRecords(), $timeZone->getCountries()->countRecords(), 'Global UTC time zone should have the same number of countries as the country collection.');
+    }
+
+    public function test_UTCTimeZoneContainsAllKnownLocales() : void
+    {
+        $timeZone = TimeZoneCollection::getInstance()->getByID(GlobalUTCTimeZone::ZONE_ID);
+
+        $this->assertInstanceOf(GlobalUTCTimeZone::class, $timeZone);
+
+        $this->assertSame(LocalesCollection::getInstance()->countRecords(), $timeZone->getLocales()->countRecords(), 'Global UTC time zone should have the same number of locales as the locale collection.');
+    }
+
+    public function test_CETContainsAllKnownEuropeanCountries() : void
+    {
+        $timeZone = TimeZoneCollection::getInstance()->getByID(GlobalCETTimeZone::ZONE_ID);
+
+        $this->assertInstanceOf(GlobalCETTimeZone::class, $timeZone);
+
+        $expected = array(
+            CountryNL::ISO_CODE,
+            CountryDE::ISO_CODE,
+            CountryFR::ISO_CODE,
+            CountryBE::ISO_CODE,
+            CountryPL::ISO_CODE,
+            CountryRO::ISO_CODE,
+            CountryIT::ISO_CODE,
+            CountryES::ISO_CODE,
+            CountryAT::ISO_CODE,
+            CountryCH::ISO_CODE,
+            CountryIE::ISO_CODE,
+            CountryGB::ISO_CODE,
+            CountryFI::ISO_CODE,
+            CountrySE::ISO_CODE,
+        );
+
+        sort($expected);
+
+        $this->assertSame($expected, $timeZone->getCountries()->getIDs());
     }
 }
