47d93d318fefd2960361c4309ce50291696a3c72
Mistralys <s.mordziol@gmail.com>
2025-07-28 11:14:31 +0200

    Add PHPDocs; Moved a method.

--- src/Localization/TimeZones/TimeZoneInterface.php
+++ src/Localization/TimeZones/TimeZoneInterface.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package Localization
+ * @subpackage TimeZones
+ */
 
 declare(strict_types=1);
 
@@ -7,6 +11,17 @@ namespace AppLocalize\Localization\TimeZones;
 use AppLocalize\Localization\Countries\CountryBasket;
 use AppUtils\Interfaces\StringPrimaryRecordInterface;
 
+/**
+ * Interface for all time zones. This includes global time zones,
+ * as well as country-specific time zones.
+ *
+ * > NOTE: Country-specific time zones implement the interface
+ * > {@see CountryTimeZoneInterface}.
+ *
+ * @package Localization
+ * @subpackage TimeZones
+ * @see CountryTimeZoneInterface
+ */
 interface TimeZoneInterface extends StringPrimaryRecordInterface
 {
     public function getLabel(): string;
@@ -16,10 +31,4 @@ interface TimeZoneInterface extends StringPrimaryRecordInterface
     public function getZoneLabelInvariant() : string;
     public function getLocationLabel() : string;
     public function getLocationLabelInvariant() : string;
-
-    /**
-     * Gets all the known countries that use this timezone.
-     * @return CountryBasket
-     */
-    public function getCountries() : CountryBasket;
 }
